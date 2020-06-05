8bec2a5500eed37c298c4c00e701183bce82d33d
ugurduyar <ismailugurduyar@gmail.com>
2020-06-05 23:34:12 +0300

    added close transition to modal

--- script.js
+++ script.js
@@ -13,3 +13,8 @@ open.addEventListener("click", () => modal.classList.add("show-modal"));
 
 // Hide modal
 close.addEventListener("click", () => modal.classList.remove("show-modal"));
+
+// Hide modal on outside click
+window.addEventListener("click", (e) =>
+  e.target == modal ? modal.classList.remove("show-modal") : flase
+);
