c5f960d81d429c46008824b65988535838d8eb4d
ugurduyar <ismailugurduyar@gmail.com>
2020-06-05 23:32:27 +0300

    modal box open animation added

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ const modal = document.getElementById("modal");
 toggle.addEventListener("click", () =>
   document.body.classList.toggle("show-nav")
 );
+
+// Show modal
+open.addEventListener("click", () => modal.classList.add("show-modal"));
+
+// Hide modal
+close.addEventListener("click", () => modal.classList.remove("show-modal"));
